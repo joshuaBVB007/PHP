7af3760e4c8b200a0fcd46d7899008f8602430da
joshuaBVB007 <56931544+joshuaBVB007@users.noreply.github.com>
2021-04-09 16:39:17 +0200

    PHP

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Mi mini pagina en php</title>
+</head>
+<body>
+    <h1>Practicando PHP</h1>
+    <?php
+    echo "mi primer script en php";
+    $minombre="Jonathan";
+    $miapellido="Calderón";
+    echo "<h1>Hola " .$minombre. "</h1>";
+    ?>
+</body>
+</html>
