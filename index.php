daa71eb79fb22c8044ecc548ed5fa2424beee083
joshuaBVB007 <56931544+joshuaBVB007@users.noreply.github.com>
2021-04-11 12:57:37 +0200

    "inserciones a bases de datos"

--- index.php
+++ index.php
@@ -6,54 +6,7 @@
 <body>
     <h1>Practicando PHP</h1>
     <?php
-    //te imprime un monton de tablas con informacion y versiones
-    //phpinfo();
-
-
-    //creacion de una variable en php
-    $minombre="Jonathan";
-    $miapellido="Calderón";
-
-    //Declaracion de booleanos en php 
-    $cierto=true;
-    $falso=false;
-
-    //echo es nuestro System.out.print de Java
-    echo "<h1>Hola " .$minombre. "</h1>";
-
-    //Creando una constante en php: las constantes en php se crean con el metodo define()
-    define("Const_1","Soy una constante");
-    echo Const_1;
-
-    //condicionales en PHP
-    $edad=19;
-    if($edad<18){
-        echo "<br>SOY IF";
-    }elseif($edad==19){
-        echo "<br>SOY ELSEIF";    
-    }else{
-        echo "SOY ELSE";  
     
-    }
-
-
-    //Bucles en PHP
-    echo "<br>SOMOS UN WHILE<br>";
-    $freno=1;
-    while(true){
-        if($freno==9){
-            echo "The number is: $freno <br>";
-            break;
-        }
-        echo "The number is: $freno <br>";
-        $freno++;
-    }
-
-    echo "<br>ESTO ES UN FOR <br>";
-    for($i=0;$i<10;$i++){
-        echo "$i";
-    }
-
     $servername = "localhost";
     $username = "root";
     $password = "";
@@ -63,11 +16,48 @@
         // set the PDO error mode to exception
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         echo "Connected successfully";
+
         } catch(PDOException $e) {
         echo "Connection failed: " . $e->getMessage();
     }
 
+        /*creamos la base de datos
+        $sql = "CREATE DATABASE myDBPDO";
+        // use exec() because no results are returned
+        $conn->exec($sql);
+        echo "Database created successfully<br>";
+        */
 
+
+    /*try {
+        //code...
+        $sql = "CREATE TABLE Futbolistas (
+            id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+            firstname VARCHAR(30) NOT NULL,
+            lastname VARCHAR(30) NOT NULL,
+            Equipo VARCHAR(50)
+            )";
+
+        if ($conn->query($sql) == TRUE) {
+            echo "Table created successfully";
+        } 
+
+    } catch (PDOException $e1) {
+        echo "Error al crear la tabla";
+    }*/
+
+
+    //crear un registro con PDO
+    /*
+    $sql = "INSERT INTO Futbolistas (firstname, lastname, Equipo)
+    VALUES ('Cristiano', 'Ronaldo', 'FC Barcelona')";
+    // use exec() because no results are returned
+    $conn->exec($sql);
+    echo "New record created successfully";
+    */
+
+    //Cerramos la conexion
+    $conn=null;
 
     ?>
 </body>
